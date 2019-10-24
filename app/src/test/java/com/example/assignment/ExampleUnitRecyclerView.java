7daab095350c2deea10530a16c8306a9aef7580b
abkhrr <bukhariadbuk@gmail.com>
2019-10-24 18:59:14 +0700

    My Assignment with octomate

---
+++ app/src/test/java/com/example/assignment/ExampleUnitRecyclerView.java
@@ -0,0 +1,48 @@
+package com.example.assignment;
+
+import android.app.Application;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.Observer;
+
+import com.example.assignment.ViewModel.FilesViewModel;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleUnitRecyclerView {
+    @Rule
+    public InstantTaskExecutorRule rule = new InstantTaskExecutorRule();
+
+    @Mock
+    private Observer<Employee> observer;
+    private Application application;
+
+    @Before
+    public void initMocks(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test_returnCorrectObject(){
+        List<Employee> employees = generateEmployees();
+        FilesViewModel viewModel = new FilesViewModel(application);
+        viewModel.getAllEmployee().getValue();
+    }
+
+    private List<Employee> generateEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            Employee employee = new Employee("name","age","position","contract");
+            employee.setName("name" + i);
+            employees.add(employee);
+        }
+        return employees;
+    }
+}
