aa2f5f36acfd94cd525df1dc0d3e63cb35d4e641
abkhrr <bukhariadbuk@gmail.com>
2019-10-25 13:48:05 +0800

    My Assignment with octomate

--- app/src/androidTest/java/com/example/assignment/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/assignment/ExampleInstrumentedTest.java
@@ -1,11 +1,24 @@
 package com.example.assignment;
 
+import android.app.Application;
 import android.content.Context;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.Observer;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.assignment.ViewModel.FilesViewModel;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
